e0dff4f684cfb7f545beeddf395d2a255e0c6dc7
maximkoo <svazarm@yandex.ru>
2019-10-17 07:31:56 +0300

    TCP chat

---
+++ s1/1.rb
@@ -0,0 +1,4 @@
+a=lambda {|x| [1,2,3]}
+
+b=a.call({});
+puts b
---
+++ tcp_stuff/tcp_server.rb
@@ -0,0 +1,9 @@
+# tcp_server.rb
+require 'socket'
+server = TCPServer.new 5678
+
+while session = server.accept
+  session.puts "Hello world! The time is #{Time.now}"
+  puts session.object_id;
+  session.close
+end
